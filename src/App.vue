c962790dcbb2c66dd39a4af90727326927a8e420
Isha Shah <ishashahjan@gmail.com>
2023-11-07 19:12:54 +0530

    reaction timer game completed

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+ <h1>Reaction Timer </h1>
+ <button @click="start" :disabled="isPlaying">Play</button>
+ <my-block :delay="delay" v-if="isPlaying" @end="endGame"></my-block>
+ <my-results :score="score" v-if="showResults"></my-results>
+</template>
+
+<script>
+
+
+import MyBlock from './components/my-block.vue';
+import MyResults from './components/my-results.vue';
+export default {
+  name: 'App',
+  data(){
+   return{
+    isPlaying: false,
+    delay: null,
+    score: null,
+    showResults: false
+   }
+  },
+  components: {
+    MyBlock,
+    MyResults
+  },
+  methods:{
+    start(){
+       this.delay = 2000 + Math.random() * 5000;
+       this.isPlaying = true;
+       console.log(this.delay);
+    },
+    endGame(reactionTime){
+      this.isPlaying = false;
+      this.score = reactionTime;
+      this.showResults = true;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  cursor: not-allowed;
+  opacity: 0.2
+}
+</style>
---
+++ src/components/my-block.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="block" v-if="showBlock" @click ="stopTimer">
+    Click Me
+  </div>
+</template>
+
+<script>
+export default {
+   props: ['delay'],
+   data(){
+    return{
+        showBlock:false,
+        timer: null,
+        reactiontime: 0
+    }
+   },
+   mounted(){
+    console.log("Component mounted");
+    setTimeout(() => {
+        this.showBlock = true;
+        this.startTimer();
+    }, this.delay);
+   },
+   updated(){
+    console.log("Component updated");
+   },
+   methods:{
+    startTimer(){
+        this.timer = setInterval(()=>{
+           this.reactiontime += 10;
+        },10) 
+    },
+    stopTimer(){
+       clearInterval(this.timer);
+       this.$emit('end',this.reactiontime);
+    }
+   }
+}
+</script>
+
+<style>
+.block{
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
